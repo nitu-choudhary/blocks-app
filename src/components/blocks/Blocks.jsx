f6ef9b64dc7ec6c324ccc42228ad0c23fa40cc78
LearningAdventuresWithNitu <142468489+LearningAdventuresWithNitu@users.noreply.github.com>
2024-03-26 03:19:43 -0400

    Integrate blocks component with backend

--- src/components/blocks/Blocks.jsx
+++ src/components/blocks/Blocks.jsx
@@ -1,21 +1,51 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../header/Header';
-import { ethereumAddresses, ethereumBlocks } from '../mockData';
 import BlockDetails from './BlockDetails';
 
 function Blocks() {
     const [selectedAddress, setSelectedAddress] = useState('');
     const [selectedBlock, setSelectedBlock] = useState({});
+    const [ethereumAddresses, setEthereumAddresses] = useState([]);
 
-    const handleSelection = (event) => {
+    useEffect(() => {
+        // fetch ethereum address from backend
+        fetchAddresses();
+    }, []);
+
+    const fetchAddresses = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/blocks/addresses');
+            if (!response.ok) {
+                throw new Error('Failed to fetch addresses');
+            }
+            const addresses = await response.json();
+            setEthereumAddresses(addresses);
+        } catch (error) {
+            console.error('Error fetching addresses:', error);
+        }
+    };
+
+    const handleSelection = async (event) => {
         // Capture the selected address
         const addressValue = event.target.value;
 
         // Set the address and block details
         setSelectedAddress(addressValue);
-        setSelectedBlock(ethereumBlocks.find((block) => block.address === addressValue));
-    };
+
+        // fetch the block details for selected ethereum address from backend
+        try {
+            const response = await fetch(`http://localhost:3000/blocks/details/${addressValue}`);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch block details for address ${addressValue}`);
+            }
+            const blockDetails = await response.json();
+            setSelectedBlock(blockDetails);
+        } catch (error) {
+            console.error(error);
+            setSelectedBlock({});
+        }
+        };
 
     return(
         <div>
